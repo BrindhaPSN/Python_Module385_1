a2ef8f216fcd36bc62069164a2bf5a5ebb245a69
BrindhaPSN <brindha.psn@gmail.com>
2026-04-22 14:15:31 -0400

    Initial commiit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Python Revision
+- to run python script `python <filename.ext>` or `python3 <filename.ext>`
+
+## Quick keys:
+- Open and close terminal `ctrl+~`
+- clearing/cleaning terminal: type `clear` or `ctrl + l`
+- to view previous terminal commands cycle `up-arrow` or `downarrow`
---
+++ welcome.py
@@ -0,0 +1,27 @@
+# Single line comments use # symbol in front of comments
+print('Welcome to Python!!!')
+
+"""
+For multi line comments use either three single quotes/three double quotes at the beginning/end of comments 
+"""
+
+print("Per Scholas is Unlocking Potential and Changing the Face of Tech")
+
+print(10+90)
+
+print(300.5+0.5)
+
+print(12, 24, -2, sep=':')
+
+print('but', 'not', 'including', sep='**')
+
+print('but', 'not', 'including', sep='')
+
+name = input('What is your name: ')
+print('Hello',name,'\n what is up?')
+
+mobile = input('Enter your mobile no:')
+homephone = input('Enter the home phone no:')
+
+print('Mobile no:',mobile)
+print('Home phone no:',homephone)
